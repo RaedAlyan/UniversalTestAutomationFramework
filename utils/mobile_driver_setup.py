32bf70a034380f3fa6a469519a192aaaf3add75a
Raed Eleyan <raedmalyan@gmail.com>
2024-10-14 13:49:25 +0300

    added some enhancements and improvements to the mobile_driver_setup.py module

--- utils/mobile_driver_setup.py
+++ utils/mobile_driver_setup.py
@@ -10,45 +10,78 @@ logger = logging.getLogger(__name__)
 
 
 class MobileDriverSetup:
-
+    """
+    A class to manage the setup and teardown of Mobile WebDriver for Appium tests.
+    """
     def __init__(self):
         self.driver = None
+        self.config = ConfigParser()
         self.appium_server_url = self.get_appium_server_url()
         self.desired_capabilities = self.get_mobile_desired_capabilities()
-        self.config = ConfigParser()
 
-    def create_driver(self):
-        """Create a mobile Driver instance based on the specified desired capabilities."""
+    def create_mobile_driver(self) -> webdriver.Remote:
+        """
+        Create a mobile WebDriver instance based on the specified desired capabilities.
+
+        :return: A mobile WebDriver instance.
+        :raises WebDriverException: If there are issues initializing the mobile WebDriver.
+        :raises Exception: For any unexpected errors during driver creation.
+        """
         try:
             logger.info('Attempting to initialize Mobile WebDriver')
             desired_capabilities = UiAutomator2Options().load_capabilities(self.desired_capabilities)
             self.driver = webdriver.Remote(self.appium_server_url, options=desired_capabilities)
             logger.info('Mobile driver successfully initialized')
+            return self.driver
         except WebDriverException as e:
-            logger.error(f'Mobile driver encountered an issue: {str(e)}')
+            logger.error(f'Mobile driver encountered an error during initialization. Error: {e}.')
             raise
         except Exception as e:
-            logger.error(f'An unexpected error occurred while initializing the mobile driver: {str(e)}')
+            logger.error(f'An unexpected error occurred while initializing the mobile driver. Error: {e}.')
             raise
-        return self.driver
 
-    def get_mobile_desired_capabilities(self):
-        """Retrieves the mobile desired capabilities from the config.json file"""
-        return self.config.get_mobile_desired_capabilities()
+    def get_mobile_desired_capabilities(self) -> dict:
+        """
+        Retrieve the mobile desired capabilities from the config.json file
 
-    def get_appium_server_url(self):
-        """Retrieves the appium server url from the config.json file"""
-        return self.config.get_mobile_appium_server()
+        :return desired_capabilities: A dictionary containing the mobile desired capabilities.
+        :raises Exception: If there is an unexpected error occurs during getting mobile desired capabilities.
+        """
+        try:
+            desired_capabilities = self.config.get_mobile_desired_capabilities()
+            return desired_capabilities
+        except Exception as e:
+            logger.error(f'An unexpected error occurred while getting mobile desired capabilities. Error: {e}')
+            raise
 
-    def quit_driver(self):
-        """Quit the Mobile Driver instance."""
-        if self.driver:
+    def get_appium_server_url(self) -> str:
+        """
+        Retrieve the appium server url from the config.json file.
+
+        :return appium_server_url: the appium server url.
+        :raises Exception: If there is an unexpected error occurs during getting appium server url.
+        """
+        try:
+            appium_server_url = self.config.get_mobile_appium_server()
+            return appium_server_url
+        except Exception as e:
+            logger.error(f'Unexpected error occurred while getting appium server url. Error: {e}')
+            raise
+
+    def quit_driver(self) -> None:
+        """
+        Quit the Mobile WebDriver instance.
+
+        :return: None.
+        :raises WebDriverException: If WebDriver encountered an unexpected error during quitting.
+        """
+        if self.driver is not None:
             try:
-                logger.info('Attempting to quit the Mobile driver instance')
+                logger.info('Quitting the Mobile WebDriver instance')
                 self.driver.quit()
-                logger.info('Mobile driver instance successfully quited')
+                logger.info('The Mobile WebDriver instance is quited successfully.')
             except WebDriverException as e:
-                logger.error(f'Failed to quit the Mobile driver instance: {str(e)}')
+                logger.error(f'Failed to quit the Mobile WebDriver. Error: {e}.')
                 raise
             finally:
                 self.driver = None
