a73e3ae3d1916d082251c033997fa88c13cf20f2
Raed Eleyan <raedmalyan@gmail.com>
2024-10-14 13:18:07 +0300

    added some enhancements and improvements to the web_driver_setup.py module

--- utils/web_driver_setup.py
+++ utils/web_driver_setup.py
@@ -1,5 +1,6 @@
 import logging
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -16,59 +17,91 @@ logger = logging.getLogger(__name__)
 
 
 class WebDriverSetup:
+    """
+    A class to manage the setup and teardown of WebDriver instances based on a specified browser.
+    """
 
     def __init__(self):
         self.driver = None
         self.browser = self.get_specified_browser()
         self.config = ConfigParser()
 
-    def get_specified_browser(self):
-        """Get the specified browser from the config.json"""
-        return self.config.get_web_browser()
+    def get_specified_browser(self) -> str:
+        """
+        Retrieve the specified browser from the config.json.
 
-    def create_driver(self):
-        """Create a WebDriver instance based on the specified browser."""
+        :return browser: The browser name (e.g., 'chrome', 'firefox', 'edge').
+        :raises ValueError: If the specified browser is not supported or specified.
+        """
+        try:
+            browser = self.config.get_web_browser().lower()
+            if browser not in ['chrome', 'firefox', 'edge']:
+                logger.error('Specified browser is not supported.')
+                raise ValueError(f'Specified browser {browser} is not supported.')
+            return browser
+        except Exception as e:
+            logger.error(f'An error occurred while trying to retrieve the specified browser. Error: {e}')
+            raise
+
+    def create_driver(self) -> WebDriver:
+        """
+        Create and initialize a WebDriver instance based on the specified browser.
+
+        :return: A WebDriver instance for the specified browser.
+        :raises Exception: For any unexpected errors during driver initialization.
+        """
         try:
             logger.info(f'Attempting to initialize WebDriver for browser: {self.browser}')
             self._initialize_driver()
             logger.info(f'{self.browser.capitalize()} WebDriver successfully initialized')
+            return self.driver
+        except Exception as e:
+            logger.error(f'An unexpected error occurred while creating the WebDriver. Error: {e}')
+            raise
+
+    def _initialize_driver(self) -> None:
+        """
+        Initialize WebDriver based on browser type.
+
+        :return: None.
+        :raises ValueError: If the specified browser is not supported.
+        :raises SessionNotCreatedException: If there is an issue with creating a session.
+        :raises WebDriverException: if WebDriver encountered an unexpected error during initialization.
+        """
+        try:
+            if self.browser == 'chrome':
+                logger.info('Initializing Chrome WebDriver')
+                self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            elif self.browser == 'edge':
+                logger.info('Initializing Edge WebDriver')
+                self.driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+            elif self.browser == 'firefox':
+                logger.info('Initializing Firefox WebDriver')
+                self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+            else:
+                logger.error(f'Unsupported browser: {self.browser}')
+                raise ValueError(f'Unsupported browser: {self.browser}')
         except SessionNotCreatedException as e:
-            logger.error(f'Session could not be created: {str(e)}')
+            logger.error(f'Session could not be created. Error: {e}')
             raise
         except WebDriverException as e:
-            logger.error(f'WebDriver encountered an issue: {str(e)}')
+            logger.error(f'WebDriver encountered an error during the initialization process. Error: {e}')
             raise
-        except ValueError as e:
-            logger.error(f'ValueError: {str(e)}')
-            raise
-        except Exception as e:
-            logger.error(f'An unexpected error occurred: {str(e)}')
-            raise
-        return self.driver
 
-    def _initialize_driver(self):
-        """Initialize WebDriver based on browser type."""
-        if self.browser == 'chrome':
-            logger.info('Initializing Chrome WebDriver')
-            self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-        elif self.browser == 'edge':
-            logger.info('Initializing Edge WebDriver')
-            self.driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
-        elif self.browser == 'firefox':
-            logger.info('Initializing Firefox WebDriver')
-            self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        else:
-            logger.error(f'Unsupported browser: {self.browser}')
-            raise ValueError(f'Unsupported browser: {self.browser}')
+    def quit_driver(self) -> None:
+        """
+        Quit the WebDriver instance, ensuring all browser sessions are properly closed.
 
-    def quit_driver(self):
-        """Quit the WebDriver instance."""
+        :return: None.
+        :raises WebDriverException: If WebDriver encountered an unexpected error during quitting.
+        """
         if self.driver is not None:
             try:
                 logger.info('Quitting the WebDriver instance')
                 self.driver.quit()
+                logger.info('The WebDriver instance is quited successfully.')
             except WebDriverException as e:
-                logger.error(f'Failed to quit the WebDriver: {str(e)}')
+                logger.error(f'Failed to quit the WebDriver. Error: {e}.')
                 raise
             finally:
                 self.driver = None
