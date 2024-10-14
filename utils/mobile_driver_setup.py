c99bca4a388668895e7abc462260e3708f82cd3f
Raed Eleyan <raedmalyan@gmail.com>
2024-10-14 16:03:34 +0300

    added some enhancements to the web and mobile driver setups after created the driver setup parent class

--- utils/mobile_driver_setup.py
+++ utils/mobile_driver_setup.py
@@ -1,21 +1,15 @@
-import logging
 from appium import webdriver
 from selenium.common.exceptions import WebDriverException
 from appium.options.android import UiAutomator2Options
-from logging_config import setup_logger
-from config_parser import ConfigParser
-
-setup_logger()
-logger = logging.getLogger(__name__)
+from driver_setup import DriverSetup
 
 
-class MobileDriverSetup:
+class MobileDriverSetup(DriverSetup):
     """
     A class to manage the setup and teardown of Mobile WebDriver for Appium tests.
     """
     def __init__(self):
-        self.driver = None
-        self.config = ConfigParser()
+        super().__init__()
         self.appium_server_url = self.get_appium_server_url()
         self.desired_capabilities = self.get_mobile_desired_capabilities()
 
@@ -28,16 +22,16 @@ class MobileDriverSetup:
         :raises Exception: For any unexpected errors during driver creation.
         """
         try:
-            logger.info('Attempting to initialize Mobile WebDriver')
+            self.logger.info('Attempting to initialize Mobile WebDriver')
             desired_capabilities = UiAutomator2Options().load_capabilities(self.desired_capabilities)
             self.driver = webdriver.Remote(self.appium_server_url, options=desired_capabilities)
-            logger.info('Mobile driver successfully initialized')
+            self.logger.info('Mobile driver successfully initialized')
             return self.driver
         except WebDriverException as e:
-            logger.error(f'Mobile driver encountered an error during initialization. Error: {e}.')
+            self.logger.error(f'Mobile driver encountered an error during initialization. Error: {e}.')
             raise
         except Exception as e:
-            logger.error(f'An unexpected error occurred while initializing the mobile driver. Error: {e}.')
+            self.logger.error(f'An unexpected error occurred while initializing the mobile driver. Error: {e}.')
             raise
 
     def get_mobile_desired_capabilities(self) -> dict:
@@ -48,10 +42,13 @@ class MobileDriverSetup:
         :raises Exception: If there is an unexpected error occurs during getting mobile desired capabilities.
         """
         try:
+            self.logger.info('Attempting to retrieve mobile desired capabilities from the config.json')
             desired_capabilities = self.config.get_mobile_desired_capabilities()
+            self.logger.info(f'Mobile desired capabilities retrieved successfully. '
+                             f'Desired capabilities: {desired_capabilities}')
             return desired_capabilities
         except Exception as e:
-            logger.error(f'An unexpected error occurred while getting mobile desired capabilities. Error: {e}')
+            self.logger.error(f'An unexpected error occurred while getting mobile desired capabilities. Error: {e}')
             raise
 
     def get_appium_server_url(self) -> str:
@@ -62,26 +59,10 @@ class MobileDriverSetup:
         :raises Exception: If there is an unexpected error occurs during getting appium server url.
         """
         try:
+            self.logger.info('Attempting to retrieve Appium server url from the config.json')
             appium_server_url = self.config.get_mobile_appium_server()
+            self.logger.info(f'Appium server url retrieved successfully. Appium server url: {appium_server_url}')
             return appium_server_url
         except Exception as e:
-            logger.error(f'Unexpected error occurred while getting appium server url. Error: {e}')
+            self.logger.error(f'Unexpected error occurred while getting Appium server url. Error: {e}')
             raise
-
-    def quit_driver(self) -> None:
-        """
-        Quit the Mobile WebDriver instance.
-
-        :return: None.
-        :raises WebDriverException: If WebDriver encountered an unexpected error during quitting.
-        """
-        if self.driver is not None:
-            try:
-                logger.info('Quitting the Mobile WebDriver instance')
-                self.driver.quit()
-                logger.info('The Mobile WebDriver instance is quited successfully.')
-            except WebDriverException as e:
-                logger.error(f'Failed to quit the Mobile WebDriver. Error: {e}.')
-                raise
-            finally:
-                self.driver = None
--- utils/web_driver_setup.py
+++ utils/web_driver_setup.py
@@ -1,4 +1,3 @@
-import logging
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,23 +7,17 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.common.exceptions import SessionNotCreatedException, WebDriverException
-from logging_config import setup_logger
-from config_parser import ConfigParser
+from driver_setup import DriverSetup
 
 
-setup_logger()
-logger = logging.getLogger(__name__)
-
-
-class WebDriverSetup:
+class WebDriverSetup(DriverSetup):
     """
-    A class to manage the setup and teardown of WebDriver instances based on a specified browser.
+    A class to manage the setup and teardown of Web WebDriver instances based on a specified browser.
     """
 
     def __init__(self):
-        self.driver = None
+        super().__init__()
         self.browser = self.get_specified_browser()
-        self.config = ConfigParser()
 
     def get_specified_browser(self) -> str:
         """
@@ -34,13 +27,14 @@ class WebDriverSetup:
         :raises ValueError: If the specified browser is not supported or specified.
         """
         try:
+            self.logger.info('Getting specified browser from the config.json')
             browser = self.config.get_web_browser().lower()
             if browser not in ['chrome', 'firefox', 'edge']:
-                logger.error('Specified browser is not supported.')
+                self.logger.error('Specified browser is not supported.')
                 raise ValueError(f'Specified browser {browser} is not supported.')
             return browser
         except Exception as e:
-            logger.error(f'An error occurred while trying to retrieve the specified browser. Error: {e}')
+            self.logger.error(f'An error occurred while trying to retrieve the specified browser. Error: {e}')
             raise
 
     def create_driver(self) -> WebDriver:
@@ -51,12 +45,12 @@ class WebDriverSetup:
         :raises Exception: For any unexpected errors during driver initialization.
         """
         try:
-            logger.info(f'Attempting to initialize WebDriver for browser: {self.browser}')
+            self.logger.info(f'Attempting to initialize Web WebDriver for browser: {self.browser}')
             self._initialize_driver()
-            logger.info(f'{self.browser.capitalize()} WebDriver successfully initialized')
+            self.logger.info(f'{self.browser.capitalize()} Web WebDriver successfully initialized')
             return self.driver
         except Exception as e:
-            logger.error(f'An unexpected error occurred while creating the WebDriver. Error: {e}')
+            self.logger.error(f'An unexpected error occurred while creating the Web WebDriver. Error: {e}')
             raise
 
     def _initialize_driver(self) -> None:
@@ -70,38 +64,20 @@ class WebDriverSetup:
         """
         try:
             if self.browser == 'chrome':
-                logger.info('Initializing Chrome WebDriver')
+                self.logger.info('Initializing Chrome WebDriver')
                 self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
             elif self.browser == 'edge':
-                logger.info('Initializing Edge WebDriver')
+                self.logger.info('Initializing Edge WebDriver')
                 self.driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
             elif self.browser == 'firefox':
-                logger.info('Initializing Firefox WebDriver')
+                self.logger.info('Initializing Firefox WebDriver')
                 self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
             else:
-                logger.error(f'Unsupported browser: {self.browser}')
+                self.logger.error(f'Unsupported browser: {self.browser}')
                 raise ValueError(f'Unsupported browser: {self.browser}')
         except SessionNotCreatedException as e:
-            logger.error(f'Session could not be created. Error: {e}')
+            self.logger.error(f'Session could not be created. Error: {e}')
             raise
         except WebDriverException as e:
-            logger.error(f'WebDriver encountered an error during the initialization process. Error: {e}')
+            self.logger.error(f'WebDriver encountered an error during the initialization process. Error: {e}')
             raise
-
-    def quit_driver(self) -> None:
-        """
-        Quit the WebDriver instance, ensuring all browser sessions are properly closed.
-
-        :return: None.
-        :raises WebDriverException: If WebDriver encountered an unexpected error during quitting.
-        """
-        if self.driver is not None:
-            try:
-                logger.info('Quitting the WebDriver instance')
-                self.driver.quit()
-                logger.info('The WebDriver instance is quited successfully.')
-            except WebDriverException as e:
-                logger.error(f'Failed to quit the WebDriver. Error: {e}.')
-                raise
-            finally:
-                self.driver = None
